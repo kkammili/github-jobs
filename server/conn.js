a9266a2d7eb2ad80d41c687159759ef8d700ba97
krishnamraju kammili <raju@krishnarajusMBP.lan>
2023-09-23 22:22:45 -0500

    rev(db): reverted database credentials for aws

--- server/conn.js
+++ server/conn.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
-    host: 'postgres.cwgb1l9uxoxe.us-east-2.rds.amazonaws.com',
+    user: 'kkrajus007',
+    host: 'githubjobs-postgre.cwgb1l9uxoxe.us-east-2.rds.amazonaws.com',
     database: 'test',
     password: 'Kkraju**7',
     port: '5432',
